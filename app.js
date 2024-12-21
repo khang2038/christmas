1f5e8feb38238ee17472670ce479c95e4bf9ab55
khang2038 <trankhang9c2003@gmail.com>
2024-12-21 09:36:19 +0700

    fix

--- app.js
+++ app.js
@@ -221,3 +221,187 @@ function resetText(){
   });
 
 });
+
+
+// Tạo hiệu ứng rơi item mà không dùng GSAP
+// const totalItems = 18;
+
+// for (let i = 1; i <= totalItems; i++) {
+//   const item = document.getElementById(`item${i}`);
+//   const duration = Math.random() * (4.5 - 3) + 3;
+//   const delay = Math.random();
+
+//   // Áp dụng hiệu ứng bằng CSS
+//   item.style.animation = `drop ${duration}s ease-out ${delay}s forwards, hang 3s infinite ease-in-out ${delay + duration}s`;
+// }
+
+// // Thêm hiệu ứng CSS vào phần tử
+// const style = document.createElement('style');
+// style.textContent = `
+//   @keyframes drop {
+//     from {
+//       transform: translateY(-100%);
+//     }
+//     to {
+//       transform: translateY(0);
+//     }
+//   }
+
+//   @keyframes hang {
+//     0%, 100% {
+//       transform: rotate(-5deg);
+//     }
+//     50% {
+//       transform: rotate(5deg);
+//     }
+//   }
+// `;
+// document.head.appendChild(style);
+
+// // Hiệu ứng tuyết rơi
+// const canvas = document.getElementById("snow");
+// const ctx = canvas.getContext("2d");
+// let width = (ctx.canvas.width = canvas.offsetWidth);
+// let height = (ctx.canvas.height = canvas.offsetHeight);
+// const snowflakes = [];
+
+// window.onresize = function () {
+//   width = ctx.canvas.width = canvas.offsetWidth;
+//   height = ctx.canvas.height = canvas.offsetHeight;
+//   snowflakes.forEach((flake) => flake.resized());
+// };
+
+// function updateSnowflakes() {
+//   snowflakes.forEach((flake) => flake.update());
+// }
+
+// function Snow() {
+//   this.x = random(0, width);
+//   this.y = random(-height, 0);
+//   this.radius = random(0.5, 3.0);
+//   this.speed = random(0.5, 2.0);
+//   this.wind = random(-0.1, 1.0);
+//   this.isResized = false;
+
+//   this.updateData = function () {
+//     this.x = random(0, width);
+//     this.y = random(-height, 0);
+//   };
+
+//   this.resized = function () {
+//     this.isResized = true;
+//   };
+// }
+
+// Snow.prototype.draw = function () {
+//   ctx.beginPath();
+//   ctx.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
+//   ctx.fillStyle = "#fff";
+//   ctx.fill();
+//   ctx.closePath();
+// };
+
+// Snow.prototype.update = function () {
+//   this.y += this.speed;
+//   this.x += this.wind;
+
+//   if (this.y > height) {
+//     if (this.isResized) {
+//       this.updateData();
+//       this.isResized = false;
+//     } else {
+//       this.y = 0;
+//       this.x = random(0, width);
+//     }
+//   }
+// };
+
+// function createSnow(count) {
+//   for (let i = 0; i < count; i++) {
+//     snowflakes.push(new Snow());
+//   }
+// }
+
+// function drawSnowflakes() {
+//   ctx.clearRect(0, 0, width, height);
+//   snowflakes.forEach((flake) => flake.draw());
+// }
+
+// function snowLoop() {
+//   drawSnowflakes();
+//   updateSnowflakes();
+//   requestAnimationFrame(snowLoop);
+// }
+
+// function random(min, max) {
+//   return Math.random() * (max - min) + min;
+// }
+
+// createSnow(200);
+// snowLoop();
+
+// // Hiển thị thông điệp với hiệu ứng chữ
+// const cartPageBottomP = document.querySelector(".cart-page-bottom p");
+// const cartPageBottomH4 = document.querySelector(".cart-page-bottom h4");
+// let textTitle = "Gửi Thư đáng yêu nhất quả đất!";
+// let charArrTitle = textTitle.split('');
+// let text = "Giáng sinh này anh không ở cạnh em được, anh biết em buồn lòng nhiều nè, anh tạo món quà nhỏ nhỏ tặng em, mong em thích. Chúc Thư của anh có một giáng sinh thật vui vẻ thật là hạnh phúc, nhớ ăn uống đầy đủ nha, ốm quá rùi nè!!, Anh thương em❤️ ";
+// let charArrContent = text.split('');
+// let currentIndexTitle = 0;
+// let currentIndexContent = 0;
+// let textIntervalTitle;
+// let textIntervalContent;
+
+// function resetText() {
+//   clearInterval(textIntervalTitle);
+//   clearInterval(textIntervalContent);
+//   cartPageBottomH4.textContent = "";
+//   cartPageBottomP.textContent = "";
+//   currentIndexTitle = 0;
+//   currentIndexContent = 0;
+// }
+
+// $(document).ready(function () {
+//   const $card = $(".card");
+//   const $bgCard = $(".bgCard");
+//   const $icon = $(".icon");
+
+//   $card.on("click", function () {
+//     $(this).toggleClass("is-opened");
+//     if ($card.hasClass("is-opened")) {
+//       textIntervalTitle = setInterval(function () {
+//         if (currentIndexTitle < charArrTitle.length) {
+//           cartPageBottomH4.textContent += charArrTitle[currentIndexTitle];
+//           currentIndexTitle++;
+//         } else {
+//           clearInterval(textIntervalTitle);
+//           textIntervalContent = setInterval(function () {
+//             if (currentIndexContent < charArrContent.length) {
+//               cartPageBottomP.textContent += charArrContent[currentIndexContent];
+//               currentIndexContent++;
+//             } else {
+//               clearInterval(textIntervalContent);
+//             }
+//           }, 100);
+//         }
+//       }, 100);
+//     } else {
+//       resetText();
+//     }
+//   });
+
+//   $(".centerer").on("click", function () {
+//     $card.fadeIn();
+//     $bgCard.fadeIn();
+//     $icon.fadeIn();
+//   });
+
+//   $(".fa-xmark").on("click", function () {
+//     $card.fadeOut();
+//     $bgCard.fadeOut();
+//     $icon.fadeOut();
+//     $card.removeClass("is-opened");
+//     resetText();
+//   });
+// });
+
